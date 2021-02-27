8a021b11126a8d6435fbf2ba0e09ad297c295735
Chris Cummer <chriscummer@me.com>
2021-02-26 23:08:25 -0800

    ASCII shall receive
    Signed-off-by: Chris Cummer <chriscummer@me.com>

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/senorprogrammer/ascii_shall_receive
+
+go 1.15
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+ 	"os"
+)
+
+func main() {
+	fmt.Println("ascii shall receive...")
+	os.Exit(0)
+}
